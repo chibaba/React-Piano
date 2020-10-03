0f135e56a25749f43091139451141979e7ed2f63
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-10-03 05:41:01 +0100

    component for logo

---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,16 @@
+import React,   {FunctionComponent, FunctionnComponent} from 'react'
+import "./style.css"
+
+export const Logo: FunctionComponent = () => {
+  return (
+    <h1 className = "logo">
+      <span role="img" aria-label="metal hand emoji">
+        []
+      </span>
+      <span role="img" aria-label="musical keyboard emoji">
+        []
+      </span>
+      <span role="img" aria-label="musical notes emoji"
+    </h1>
+  )
+}

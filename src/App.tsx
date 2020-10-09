c58b203c1fdf247a95f308a2ad4aa1e19ef358af
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-10-09 10:44:17 +0100

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
-import { Footer } from "./components/Footer "
-import { Logo } from './components/Logo'
+import { Footer } from './components/Footer/Footer'
+import { Logo } from './components/Logo/Logo'
 import './App.css';
 
+
+export const  App = () => {
+  return (
+    <div className="app">
+      <Logo />
+      <main className="app-content" />
+
+      <Footer />
+    </div>
+  )
+}
 
 export default App;
